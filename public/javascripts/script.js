376f3fea791ad2e6cc482073002e1c6814f317c3
Mr.X <seniorito.x@icloud.com>
2019-02-12 18:04:24 +0100

    Added form and added post method that adds position to Marker model

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,55 +1,48 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-
-    console.log("IronGenerator JS imported successfully!");
-
-    mapboxgl.accessToken =
-    "pk.eyJ1Ijoic2VuaW9yaXRveCIsImEiOiJjanJ5cmQxc3gweGx5M3ludjVwamMzam1wIn0.c91HFhhkAeZWsT4v1Pm8NQ";
-    
-    let currentPosition = []
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=>{
-
-        var map = new mapboxgl.Map({
-          container: "map", // container id
-          style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
-          center: [position.coords.longitude, position.coords.latitude], // starting position [lng, lat]
-          zoom: 12, // starting zoom
-          trackuserlocationstart: true
-        });
-    
-        map.addControl(new mapboxgl.GeolocateControl({
-          positionOptions: {
-          enableHighAccuracy: true
-          },
-          trackUserLocation: true, showUserLocation: true
-          }));
-  
-        currentPosition.push(position.coords.longitude, position.coords.latitude)
-
-        var geocoder = new MapboxGeocoder({
-          accessToken: mapboxgl.accessToken
-          });
-
-        document.getElementById('geocoder').appendChild(geocoder.onAdd(map))
-        
-        map.on('click', function (e) {
-          document.getElementById('info').innerHTML =
-          // e.point is the x, y coordinates of the mousemove event relative
-          // to the top-left corner of the map
-          JSON.stringify(e.point) + '<br />' +
-          // e.lngLat is the longitude, latitude geographical position of the event
-          JSON.stringify(e.lngLat);
-          });
-
+var promise = new Promise(function(resolve, reject) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    resolve(
+      (pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
       })
-      
-    } else {
-      console.log('error at getting your position')
-    }
+    );
+  });
+});
 
-  },
-  false
-);
+promise.then(currentPosition => {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2VuaW9yaXRveCIsImEiOiJjanJ5cmQxc3gweGx5M3ludjVwamMzam1wIn0.c91HFhhkAeZWsT4v1Pm8NQ";
+
+  var map = new mapboxgl.Map({
+    container: "map", // container id
+    style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
+    center: [currentPosition.lng, currentPosition.lat], // starting position [lng, lat]
+    zoom: 14, // starting zoom
+    trackuserlocationstart: true
+  });
+
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    })
+  );
+
+  var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+  });
+
+  document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
+
+  let clickedPos = new mapboxgl.Marker();
+
+  map.on("click", function(e) {
+    clickedPos.setLngLat(e.lngLat).addTo(map);
+    document.querySelector("#lat").value = e.lngLat.lat;
+    document.querySelector("#lng").value = e.lngLat.lng;
+  });
+});
+
+
